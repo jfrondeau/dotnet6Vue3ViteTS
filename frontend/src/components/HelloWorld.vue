50a43e8278347896e1a5eef2e900495acf585ce5
Jean-Francois Rondeau <jf-rondeau@hotmail.com>
2022-01-29 10:37:30 -0500

    show data from backend api

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,25 +1,46 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue"
+import { computed, onMounted, ref } from "vue"
 
 const API_URL = `weatherforecast`;
+
+interface weatherforecast {
+  date: Date,
+  temperatureC: Number,
+  temperatureF: Number,
+  summary: String
+}
 
 defineProps<{
   msg: string
 }>()
 
-const data = ref(null)
+const data = ref<weatherforecast[]>([]);
 
 onMounted(async () => {
   data.value = await (await fetch(API_URL)).json()
 
   console.log("Component created")
   console.log("data fetched: ", data.value)
-});
+})
+
+function formatDate(d: string) {
+  return d.replace(/T|Z/g, ' ');
+}
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
+
+    <div v-if="data">
+      Some data!
+      <ul>
+        <li
+          v-for="row in data"
+        >{{ formatDate(row.date) }} | {{ row.temperatureC }} | {{ row.summary }}</li>
+      </ul>
+    </div>
+
     <h3>
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
